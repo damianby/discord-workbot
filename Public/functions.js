cdf09fc31b0219b06832a4ebc8b8bc187c6b16bb
damian_baldyga <baldygadamian@gmail.com>
2021-06-04 14:15:55 +0200

    Fix reports page and change how time is displayed

--- Public/functions.js
+++ Public/functions.js
@@ -1,36 +1,58 @@
 exports = module.exports = {};
 
 function minTwoDigits(n) {
-    return (n < 10 ? '0' : '') + n;
+	return (n < 10 ? '0' : '') + n;
 }
 
 function formatDuration(schedule) {
-    return new Date(schedule.duration * 1000).toISOString().substr(11, 8);
+	return new Date(schedule.duration * 1000).toISOString().substr(11, 8);
+}
+
+exports.formatToMonthHours = function(duration) {
+	let totalHours = duration / 60 / 60;
+
+	let hours = Math.floor(totalHours);
+
+	let minutes = Math.round((totalHours - hours) * 60);
+
+	return hours + ' godzin  ' + minutes + ' minut';
+}
+
+exports.secondsToTime = function(seconds) {
+	return new Date(seconds * 1000).toISOString().substr(11, 8);
 }
 
 exports.formatTime = function(schedule, report) {
-    
-    let time = minTwoDigits(schedule.hour) + ":" + minTwoDigits(schedule.minute) + " - " + minTwoDigits(schedule.hourEnd) + ":" + minTwoDigits(schedule.minuteEnd);
-    let date = minTwoDigits(schedule.day) + "." + minTwoDigits(report.month);
-    let duration = formatDuration(schedule);
-    
-    return date + "\t\t" + time + "\t\tCałkowity czas: " + duration;
-}
+
+	let time = minTwoDigits(schedule.hour) + ':' + minTwoDigits(schedule.minute) + ' - ' + minTwoDigits(schedule.hourEnd) + ':' + minTwoDigits(schedule.minuteEnd);
+	let date = minTwoDigits(schedule.day) + '.' + minTwoDigits(report.month);
+	let duration = formatDuration(schedule);
+
+	return date + '\t\t' + time + '\t\tCałkowity czas: ' + duration;
+};
 
 
-exports.splitByDay = function(report) {
-    let schedules = report.schedules;
+exports.reparseDaysAndDurations = function(report) {
+	let schedules = report.schedules;
 
-    let outSchedules = [];
-    for(let i = 0 ; i < schedules.length ; i++) {
-        if(outSchedules.length > 0 && outSchedules[outSchedules.length - 1].dayStart == schedules[i].day) {
-            outSchedules[outSchedules.length - 1].schedules.push(schedules[i]);
-        } else {
-            outSchedules.push({
-                dayStart: schedules[i].day,
-                schedules: [schedules[i]]
-            });
-        }
-    }
-    return outSchedules;
-}
+	let totalMonthDuration = 0;
+	let outSchedules = [];
+	for(let i = 0 ; i < schedules.length ; i++) {
+		if(outSchedules.length > 0 && outSchedules[outSchedules.length - 1].dayStart == schedules[i].day) {
+			const currentSchedule = outSchedules[outSchedules.length - 1];
+			currentSchedule.totalDuration += schedules[i].duration;
+			currentSchedule.schedules.push(schedules[i]);
+		} else {
+			outSchedules.push({
+				dayStart: schedules[i].day,
+				totalDuration: schedules[i].duration,
+				schedules: [schedules[i]],
+			});
+		}
+		totalMonthDuration += schedules[i].duration;
+	}
+	report.totalMonthDuration = totalMonthDuration;
+
+	report.schedules = outSchedules;
+	return report;
+};
--- modules/manager.js
+++ modules/manager.js
@@ -1,15 +1,15 @@
 
-const log = require("./log")('manager');
+const log = require('./log')('manager');
 
 const { v4: uuidv4 } = require('uuid');
 
-const IP = "192.168.1.114";
+const IP = '192.168.1.114';
 const PORT = 43000;
 
-let reports = {};
+const reports = {};
 
 exports.initialize = function() {
-  
+
 }
 
 exports.updateUsers = function(guildMembers) {
@@ -23,7 +23,7 @@ exports.createUser = function() {
 exports.getOneTimeReport = function(id) {
 
 	if(reports[id]) {
-		let report = reports[id];
+		const report = reports[id];
 		delete reports[id];
 		return report;
 	} else {
@@ -32,11 +32,13 @@ exports.getOneTimeReport = function(id) {
 }
 
 exports.generateOneTimeReport = function(reportData) {
-	let uuid = uuidv4().split("-").join("");
+	let uuid = uuidv4().split('-').join('');
 
-	console.log(uuid);
+	//console.log(uuid);
 
 	reports[uuid] = reportData;
 
-	return "http://" + IP + ":" + PORT.toString() + "/report/" + uuid;
+	//console.log(reportData);
+
+	return 'http://' + IP + ':' + PORT.toString() + '/report/' + uuid;
 }
